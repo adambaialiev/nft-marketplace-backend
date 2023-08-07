26db51bffaa5c0018921d176555d17e1a40c61d0
adambaialiev <adam.baialiev@gmail.com>
2023-08-07 16:31:54 +0600

    implement getUploadUrl endpoint

--- src/endpoints/nft/getUploadUrl.ts
+++ src/endpoints/nft/getUploadUrl.ts
@@ -1,19 +1,37 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { sendResponse } from "@/utils/makeResponse";
+import { S3 } from "aws-sdk";
+const KSUID = require("ksuid");
 
 export interface GetUploadUrlParams {
   extension: string;
   contentType: string;
 }
 
+const bucketName = process.env.bucket;
+
+const URL_EXPIRATION_SECONDS = 300;
+
 export const main: APIGatewayProxyHandler = async (event) => {
   try {
-    console.log({ event });
-    // const { extension, contentType } = JSON.parse(
-    //   event.body
-    // ) as GetUploadUrlParams;
+    const { extension, contentType } = JSON.parse(
+      event.body
+    ) as GetUploadUrlParams;
 
-    return sendResponse(201, true);
+    const client = new S3();
+    const Key = `${KSUID.randomSync().string}.${extension}`;
+
+    const params = {
+      Bucket: bucketName,
+      Key,
+      Expires: URL_EXPIRATION_SECONDS,
+      ContentType: contentType,
+      ACL: "public-read",
+      ContentDisposition: `attachment; filename="${Key}"`,
+    };
+    const uploadUrl = await client.getSignedUrlPromise("putObject", params);
+
+    return sendResponse(201, { uploadUrl, key: Key });
   } catch (error: unknown) {
     console.log(error);
     if (error instanceof Error) {
