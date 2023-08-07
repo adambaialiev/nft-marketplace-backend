6b2050bb4c062f625b37d2b0cc9205e16f8ee57b
adambaialiev <adam.baialiev@gmail.com>
2023-08-07 16:43:26 +0600

    implement nft collection endpoint

--- src/endpoints/nft/createNft.ts
+++ src/endpoints/nft/createNft.ts
@@ -1,10 +1,35 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { sendResponse } from "@/utils/makeResponse";
+import AWS from "aws-sdk";
+import { Entities, NFTAttributes, TableKeys } from "@/common/dynamo/schema";
+import { buildNftKey } from "@/common/dynamo/buildKey";
+const KSUID = require("ksuid");
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+const TableName = process.env.dynamo_table as string;
 
 export const main: APIGatewayProxyHandler = async (event) => {
   try {
-    // const { name, tag } = JSON.parse(event.body);
-    console.log({ event });
+    const { name, tag } = JSON.parse(event.body);
+
+    const id = KSUID.randomSync().string;
+    const Item = {
+      [TableKeys.PK]: Entities.NFT,
+      [TableKeys.SK]: buildNftKey(id),
+      [NFTAttributes.ID]: id,
+      [NFTAttributes.NAME]: name,
+      [NFTAttributes.TAG]: tag,
+      [NFTAttributes.CREATED_AT]: Date.now().toString(),
+    };
+
+    await dynamo
+      .put({
+        Item,
+        TableName,
+        ConditionExpression: `attribute_not_exists(${TableKeys.SK})`,
+      })
+      .promise();
 
     return sendResponse(201, true);
   } catch (error: unknown) {
---
+++ src/endpoints/nft/getNftCollection.ts
@@ -0,0 +1,34 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { sendResponse } from "@/utils/makeResponse";
+import AWS from "aws-sdk";
+import { Entities, TableKeys } from "@/common/dynamo/schema";
+const KSUID = require("ksuid");
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+const TableName = process.env.dynamo_table as string;
+
+export const main: APIGatewayProxyHandler = async (event) => {
+  try {
+    const queryOutput = await dynamo
+      .query({
+        TableName,
+        KeyConditionExpression: "#pk = :pk",
+        ExpressionAttributeNames: {
+          "#pk": TableKeys.PK,
+        },
+        ExpressionAttributeValues: {
+          ":pk": Entities.NFT,
+        },
+        ScanIndexForward: false,
+      })
+      .promise();
+
+    return sendResponse(201, queryOutput.Items);
+  } catch (error: unknown) {
+    console.log(error);
+    if (error instanceof Error) {
+      return sendResponse(500, error.message);
+    }
+  }
+};
